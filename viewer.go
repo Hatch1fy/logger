347dbba95e917250a7e31ba7738c60959d956114
Josh Montoya <josh@montoya.io>
2018-09-19 20:39:26 -0700

    Improve commenting

--- viewer.go
+++ viewer.go
@@ -23,15 +23,21 @@ type Viewer struct {
 
 // ForEach will iterate through all the available logs
 func (v *Viewer) ForEach(fn func(key string) (err error)) (err error) {
+	// Walk through each file in the set directory
 	err = filepath.Walk(v.dir, func(filepath string, info os.FileInfo, ierr error) (err error) {
+		// If we're looking at a directory, we're definitely not looking at a log file
 		if info.IsDir() {
+			// This is a directory, return
 			return
 		}
 
+		// Ensure Iterating log is a target log
 		if strings.Index(filepath, v.name) == -1 {
+			// Not a target log, return
 			return
 		}
 
+		// Pass filepath provided iterating function
 		return fn(filepath)
 	})
 
