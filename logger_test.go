412e4580c6ff0f679fe83ef93dd745a6e9cc5f40
Josh Montoya <josh@montoya.io>
2018-09-19 20:20:20 -0700

    Implement time rotation test

--- logger_test.go
+++ logger_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 )
 
 const (
@@ -30,6 +31,72 @@ func TestLogger(t *testing.T) {
 
 	if err = testLogs(l, 3); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestTimeRotation(t *testing.T) {
+	var (
+		l *Logger
+		v *Viewer
+
+		logCount  int
+		lineCount int
+
+		err error
+	)
+
+	if err = os.MkdirAll(testDir, 0744); err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(testDir)
+
+	if l, err = New(testDir, testName); err != nil {
+		t.Fatal(err)
+	}
+
+	l.SetRotateInterval(time.Second)
+
+	for i := 0; i < 6; i++ {
+		log := fmt.Sprintf("#%d", i+1)
+		if err = l.LogString(log); err != nil {
+			return
+		}
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	if err = l.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	if v, err = NewViewer(testDir, testName); err != nil {
+		t.Fatal(err)
+	}
+
+	if err = v.ForEach(func(key string) (err error) {
+		var r *Reader
+		if r, err = NewReader(key); err != nil {
+			return
+		}
+
+		if err = r.ForEach(func(ts time.Time, log []byte) (err error) {
+			lineCount++
+			expected := fmt.Sprintf("#%d", lineCount)
+			if strLog := string(log); strLog != expected {
+				return fmt.Errorf("invalid log, expected \"%s\" and received \"%s\"", expected, strLog)
+			}
+
+			return
+		}); err != nil {
+			return
+		}
+		logCount++
+		return
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	if logCount != 3 {
+		t.Fatalf("invalid number of logs, expected %d and received %d", 3, logCount)
 	}
 }
 
