fcda7565877319648e3aef691e3fcb617ffdc264
Josh Montoya <josh@montoya.io>
2018-11-08 18:03:58 -0800

    Add snapshot locking and close method

--- snapshotter.go
+++ snapshotter.go
@@ -4,8 +4,11 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"sync"
 
+	"github.com/Hatch1fy/errors"
 	"github.com/Hatch1fy/snapshotter"
+	"github.com/PathDNA/atoms"
 )
 
 // NewSnapshotter will return a new instance of Snapshotter
@@ -19,10 +22,14 @@ func NewSnapshotter(be snapshotter.Backend, deleteOnSnapshot bool) (ss *Snapshot
 
 // Snapshotter manages logger snapshots
 type Snapshotter struct {
+	mu sync.Mutex
+
 	// Backend to snapshot to
 	be snapshotter.Backend
 	// Whether or not to delete a file after it's been successfully
 	deleteOnSnapshot bool
+
+	closed atoms.Bool
 }
 
 // snapshot will perform a snapshot of the given file
@@ -47,6 +54,14 @@ func (s *Snapshotter) snapshot(filename string) (err error) {
 
 // Snapshot will perform a snapshot of the given file
 func (s *Snapshotter) Snapshot(filename string) (err error) {
+	// Acquire snapshot until snapshot has completed
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.closed.Get() {
+		return errors.ErrIsClosed
+	}
+
 	// Perform snapshot
 	if err = s.snapshot(filename); err != nil {
 		return
@@ -59,4 +74,16 @@ func (s *Snapshotter) Snapshot(filename string) (err error) {
 
 	// Remove file
 	return os.Remove(filename)
+}
+
+// Close will close an instance of snapshotter
+func (s *Snapshotter) Close() (err error) {
+	if !s.closed.Set(true) {
+		return errors.ErrIsClosed
+	}
+
+	// Acquire lock to ensure any current snapshot finished
+	s.mu.Lock()
+	s.mu.Unlock()
+	return
 }
